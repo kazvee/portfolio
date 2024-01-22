2eba15117ae1088904ee9c2ee731d7fc97f632da
kazvee <kazveedev@gmail.com>
2024-01-21 19:07:46 -0700

    feat(stats): update page to display stats section

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
+import Stats from './components/Stats';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col container mx-auto px-12 py-4">
+    <main className='flex min-h-screen flex-col container mx-auto px-12 py-4'>
       <Navbar />
-      <div className="container mt-12 mx-auto px-12 py-4">
+      <div className='container mt-12 mx-auto px-12 py-4'>
         <Header />
         <About />
         <Projects />
+        <Stats />
       </div>
       <Footer />
     </main>
