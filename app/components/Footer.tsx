a21f08b5571ef762adce52f161364243ad170d62
kazvee <kazveedev@gmail.com>
2024-01-27 21:42:53 -0700

    feat(footer): add visitor counter to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,19 +1,24 @@
-import Image from 'next/image';
-import linkedinIcon from '@/public/images/linkedin-icon.png';
 import githubIcon from '@/public/images/github-icon.png';
+import linkedinIcon from '@/public/images/linkedin-icon.png';
+import Image from 'next/image';
 import Link from 'next/link';
+import Counter from './Counter';
 
 const Footer: React.FC = () => {
   return (
     <footer className="footer border z-10 border-t-[#B6688A] border-l-transparent border-r-transparent text-pink-500">
       <div className="container p-0 md:p-12 flex justify-between items-center">
+        <span className="flex flex-row">
+          <Counter />
+        </span>
         <span>
           Chickadee photo by{' '}
           <Link
             className="text-white font-bold"
             href="https://www.pexels.com/photo/black-capped-chickadee-bird-on-a-redcurrant-shrub-19097681/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             teyi 徐
           </Link>
         </span>
@@ -23,7 +28,8 @@ const Footer: React.FC = () => {
             className="text-white font-bold"
             href="https://icons8.com/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             icons8
           </Link>
         </span>
@@ -31,13 +37,15 @@ const Footer: React.FC = () => {
           <Link
             href="https://www.linkedin.com/in/kazvee/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <Image src={linkedinIcon} alt="LinkedIn icon" />
           </Link>
           <Link
             href="https://github.com/kazvee/"
             target="_blank"
-            rel="noopener noreferrer">
+            rel="noopener noreferrer"
+          >
             <Image src={githubIcon} alt="GitHub icon" />
           </Link>
         </span>
