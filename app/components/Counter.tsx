deb4ad201b25fd8887da3f349489f0e695fbb49b
kazvee <kazveedev@gmail.com>
2024-01-27 21:51:07 -0700

    fix(counter): update visitor count image URL in counter component

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -2,7 +2,7 @@
 const Counter: React.FC = () => {
   return (
     <img
-      src="https://hits.sh/kazvee.com.svg?style=plastic&label=Visitors%3A&color=00000000&labelColor=00000000"
+      src="https://hits.sh/kazvee.com/hits.svg?style=plastic&label=Visitors%3A&color=00000000&labelColor=00000000"
       alt="Visitor Count"
       width={75}
       height={50}
