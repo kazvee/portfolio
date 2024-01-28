91a251f5cba09838505f4230b8829e66d9dbe711
kazvee <kazveedev@gmail.com>
2024-01-27 21:33:50 -0700

    feat(counter): add visitor counter component

---
+++ app/components/Counter.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @next/next/no-img-element */
+const Counter: React.FC = () => {
+  return (
+    <img
+      src="https://hits.sh/kazvee.com.svg?style=plastic&label=Visitors%3A&color=00000000&labelColor=00000000"
+      alt="Visitor Count"
+      width={75}
+      height={50}
+    />
+  );
+};
+
+export default Counter;
