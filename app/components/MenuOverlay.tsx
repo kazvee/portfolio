c3aa6f9d2fb3838c4feb0d28b11f9233fbbdd3cb
kazvee <kazveedev@gmail.com>
2024-10-25 09:54:28 -0600

    feat(MenuOverlay): update to close mobile menu overlay on link click

--- app/components/MenuOverlay.tsx
+++ app/components/MenuOverlay.tsx
@@ -3,13 +3,14 @@ import Navlink from './Navlink';
 
 interface MenuOverlayProps {
   links: { path: string; title: string; hoverTextColor: string }[];
+  closeOverlay: () => void;
 }
 
-const MenuOverlay: React.FC<MenuOverlayProps> = ({ links }) => {
+const MenuOverlay: React.FC<MenuOverlayProps> = ({ links, closeOverlay }) => {
   return (
     <ul className="flex flex-col py-4 items-center">
       {links.map((link, index) => (
-        <li key={index}>
+        <li key={index} onClick={closeOverlay}>
           <Navlink
             href={link.path}
             title={link.title}
