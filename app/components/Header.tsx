213fd6c30eef6f67553092001288c6365c73e725
kazvee <kazveedev@gmail.com>
2024-10-24 21:00:37 -0600

    feat(styling): make image border visible on mobile view

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -22,7 +22,7 @@ const Header: React.FC = () => {
           <div>
             <Link
               href="https://www.linkedin.com/in/kazvee/"
-              target="_blank" 
+              target="_blank"
               rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full text-white mt-3">
               <span className="block bg-gradient-to-br from-pink-900 to-pink-700 hover:from-pink-700 hover:to-pink-900 rounded-full px-5 py-2">
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
             </Link>
             <Link
               href="https://github.com/kazvee/"
-              target="_blank" 
+              target="_blank"
               rel="noopener noreferrer"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full text-white mt-3">
               <span className="block bg-gradient-to-br from-pink-900 to-pink-700 hover:from-pink-700 hover:to-pink-900 rounded-full px-5 py-2">
@@ -41,13 +41,11 @@ const Header: React.FC = () => {
           </div>
         </div>
         <div className="col-span-4 place-self-center mt-8 lg:ml-8 sm:mt-2 sm:ml-0">
-          <div className="rounded-full bg-pink-900 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-pink-900 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative flex items-center justify-center">
             <Image
               src={headerImage}
               alt="black-capped chickadee bird sitting on a redcurrant shrub"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-              width={380}
-              height={380}
+              className="w-[95%] h-auto border-4 border-pink-900 rounded-full"
               priority={true}
             />
           </div>
