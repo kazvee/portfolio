48bf63852fd6a5a4c15ae6ccaa6fd5a67b6b12d3
kazvee <kazveedev@gmail.com>
2024-10-30 12:55:45 -0600

    fix(MenuOverlay): add role prop & fix a11y issues

--- app/components/MenuOverlay.tsx
+++ app/components/MenuOverlay.tsx
@@ -4,21 +4,29 @@ import Navlink from './Navlink';
 interface MenuOverlayProps {
   links: { path: string; title: string; hoverTextColor: string }[];
   closeOverlay: () => void;
+  role?: string;
 }
 
-const MenuOverlay: React.FC<MenuOverlayProps> = ({ links, closeOverlay }) => {
+const MenuOverlay: React.FC<MenuOverlayProps> = ({ links, closeOverlay, role }) => {
   return (
-    <ul className="flex flex-col py-4 items-center">
-      {links.map((link, index) => (
-        <li key={index} onClick={closeOverlay}>
-          <Navlink
-            href={link.path}
-            title={link.title}
-            hoverTextColor={link.hoverTextColor}
-          />
-        </li>
-      ))}
-    </ul>
+    <div
+      role={role}
+      className="flex flex-col py-4 items-center"
+      aria-labelledby="menu-title"
+    >
+      <h2 id="menu-title" className="sr-only">Navigation Menu</h2>
+      <ul className="flex flex-col py-4 items-center">
+        {links.map((link, index) => (
+          <li key={index} onClick={closeOverlay}>
+            <Navlink
+              href={link.path}
+              title={link.title}
+              hoverTextColor={link.hoverTextColor}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
