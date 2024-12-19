de8c780b6a90eb0a35e959d7607e31dce9c66270
kazvee <kazveedev@gmail.com>
2024-12-19 14:34:00 -0700

    feat(Snowfall): add Snowfall component

---
+++ app/components/Snowfall.tsx
@@ -0,0 +1,51 @@
+'use client';
+import { useEffect } from 'react';
+
+const Snowfall = () => {
+    useEffect(() => {
+        const snowEmoji = "❄️";
+        let timeoutId: number | null = null;
+
+        const addEmoji = (e: MouseEvent) => {
+            if (Math.random() > 0.2) return;
+            if (timeoutId) return;
+
+            const emoji = document.createElement('div');
+            emoji.classList.add('snowflake');
+            emoji.textContent = snowEmoji;
+
+            const maxWidth = window.innerWidth * 0.9;
+            const offsetX = Math.random() * maxWidth;
+            emoji.style.left = `${offsetX}px`;
+
+            emoji.style.top = `-${20 + Math.random() * 30}px`;
+
+            const size = Math.random() * 2 + 1.5;
+            emoji.style.fontSize = `${size}em`;
+
+            document.body.appendChild(emoji);
+
+            emoji.classList.add('fall');
+
+            setTimeout(() => emoji.remove(), 10000);
+
+            timeoutId = window.setTimeout(() => {
+                timeoutId = null;
+            }, 1000);
+        };
+
+        document.body.addEventListener('mousemove', addEmoji);
+
+        return () => {
+            document.body.removeEventListener('mousemove', addEmoji);
+
+            if (timeoutId) {
+                clearTimeout(timeoutId);
+            }
+        };
+    }, []);
+
+    return null;
+};
+
+export default Snowfall;
