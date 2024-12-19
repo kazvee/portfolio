be3f8974fc7642ea802063d6cfef2e7c2026699f
kazvee <kazveedev@gmail.com>
2024-12-19 14:34:26 -0700

    feat(Snowfall): implement Snowfall component

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Projects from './components/Projects';
 import Stats from './components/Stats';
+import Snowfall from './components/Snowfall';
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col container mx-auto px-4 md:px-12 py-4'>
+      <Snowfall />
       <Navbar />
       <div className='container mt-12 mx-auto px-4 md:px-12 py-4'>
         <Header />
