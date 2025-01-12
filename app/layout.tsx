07107818f0ba5c33b8db7b5a60089e32867f4e6a
kazvee <kazveedev@gmail.com>
2025-01-12 10:50:34 -0700

    feat(Metadata): update metadata for SEO & social sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,37 @@ import './globals.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Karen V. - Portfolio',
-  description:
-    'Global Digital Citizen ~ Lifelong Learner ~ Full Stack Software Developer',
+  title: 'Karen V. (kazvee) - Portfolio',
+  description: 'Software Developer based in Canada with a strong background in software development, technical writing, and customer-focused technical support.',
+  metadataBase: new URL('https://kazvee.com/'),
+  keywords: ['Software Developer', 'Web Developer', 'Software Engineering', 'Technical Writer', 'Portfolio', 'Canada', 'kazvee', 'Karen V.'],
+  openGraph: {
+    title: 'Karen V. (kazvee) - Portfolio',
+    description: 'IT Professional based in Canada with a background in software development, technical writing, and user-focused tech support. Explore my portfolio to learn more.',
+    url: 'https://kazvee.com/',
+    images: [
+      {
+        url: '/images/social-media-preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'Preview of Karen V. (kazvee) portfolio featuring a dark purple background, chickadee logo, and showcasing projects and programming languages.',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Karen V. (kazvee) - Portfolio',
+    description: 'IT Professional based in Canada with a background in software development, technical writing, and user-focused tech support. Explore my portfolio to learn more.',
+    images: [
+      {
+        url: '/images/social-media-preview.png',
+        width: 1200,
+        height: 630,
+        alt: 'Preview of Karen V. (kazvee) portfolio featuring a dark purple background, chickadee logo, and showcasing projects and programming languages.',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
