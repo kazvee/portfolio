50f765e57414621bae1092889b03e35cbbdd85c1
kazvee <kazveedev@gmail.com>
2025-02-16 14:57:36 -0700

    feat(api): create GET route for projects

---
+++ app/api/projects/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import db from '@/backend/connection';
+
+export async function GET() {
+    try {
+        const { rows } = await db.query('SELECT * FROM projects ORDER BY id ASC');
+        return NextResponse.json({ projects: rows });
+    } catch (error) {
+        let errorMessage = 'Unknown error occurred';
+
+        if (error instanceof Error) {
+            errorMessage = error.message;
+        }
+
+        console.error('Database error:', errorMessage);
+        return NextResponse.json({ error: 'Failed to fetch data from the database ☹️' }, { status: 500 });
+    }
+}
