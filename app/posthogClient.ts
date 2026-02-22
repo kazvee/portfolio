94b0f6c6dbcf6d119a78d7ebb161df60e659ae05
kazvee <109990289+kazvee@users.noreply.github.com>
2026-02-22 10:18:41 -0700

    fix(posthog): skip init if consent is undecided

--- app/posthogClient.ts
+++ app/posthogClient.ts
@@ -2,13 +2,14 @@ import posthog from 'posthog-js';
 
 export const initPostHog = (consent: 'yes' | 'no' | 'undecided') => {
   const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY!;
+  if (consent === 'undecided') return;
 
   posthog.init(posthogKey, {
     api_host: 'https://omg.kazvee.com',
     ui_host: 'https://us.posthog.com',
     defaults: '2025-05-24', // Enable history_change tracking
     persistence: consent === 'yes' ? 'localStorage+cookie' : 'memory',
-    autocapture: true,
+    autocapture: consent === 'yes',
     disable_session_recording: consent !== 'yes',
   });
 
